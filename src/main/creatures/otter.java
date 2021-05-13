887023d8147e6110e5da5fd933139313e3f04d2d
NoahWhit <WhiteNJ21@brightonk12.com>
2021-05-13 08:39:36 -0400

    Spell "RED": LSTER

--- src/main/creatures/otter.java
+++ src/main/creatures/otter.java
@@ -1,4 +1,8 @@
 package creatures;
 
-public class otter {
+public class otter extends AquaticMammal {
+    public void method2() { System.out.println("Paws:"); }
+    public String toString() {
+        return "Beach Dweller";
+    }
 }
